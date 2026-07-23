45cc53008d48e97251d0e71e1651299c890763f4
sylvesterleblac-code <sylvesterleblac@gmail.com>
2026-07-23 09:36:36 +0300

    Auto-commit: fileio, fileio.c at 7/23/2026, 9:36:36 AM

--- fileio.c
+++ fileio.c
@@ -19,6 +19,8 @@ Streams are categorized into two:
 Text files are logicaly a sequence of lines separatd by newlines. To be portable, your input data should always end with a newline
 NB:/- A general rule is that if you are able to edit the file in a regular text editor, it's a text file. Otherwise, it's binary.
 How do we open a file and pput text in it? Check below :)
+
+
 #include<stdio.h>
 
 int main()
@@ -31,8 +33,7 @@ int main()
     int c = fgetc(fp);       // This reads as ignle character
     // Why did we use int c instead of a char c?
     //fgetc() is to get a character from the stream
-
-    printf("%c\n", c);      // This prints char to the stdout
+    printf("%c\n%c\n", c);      // This prints char to the stdout
 
 
     fclose(fp);         // This closes te file when done
@@ -45,5 +46,30 @@ int main()
 
 
 //EOF
-There is a special character defined as a macro: EOF. THis is wha the fgetc() will return when the end of the file has been reached and you've attempted to read another character
+// There is a special character defined as a macro: EOF. THis is wha the fgetc() will return when the end of the file has been reached and you've attempted to read another character
+
 */
+
+
+#include<stdio.h>
+
+int main()
+{
+    FILE *fp;   // Variable torepresent open file
+    //fopen() returns the FILE* to us so we can use it later
+    fp = fopen("hello.txt", "r");       // This opens the file for reading
+    // The 'r' means 'open a text stream for reading'
+
+    int c = fgetc(fp);       // This reads as ignle character
+    // Why did we use int c instead of a char c?
+    //fgetc() is to get a character from the stream
+    printf("%c\n%c\n", c);      // This prints char to the stdout
+
+
+    fclose(fp);         // This closes te file when done
+    //We close the stream when we are done with it. All streams are automatically closed when the program exits but it is a good form and good housekeeping ie it is strongly advised to explicitly close any files yourself when done with it
+
+    // FILE* keeps track of our position in the file so subsequent calls to fgetc() would get the next character in the file, and then next, until the end
+
+    
+}
