2c56814b0f54662188b59976e6f38b655944e0b1
sylvesterleblac-code <sylvesterleblac@gmail.com>
2026-07-23 09:17:33 +0300

    Auto-commit: fileio.c at 7/23/2026, 9:17:33 AM

---
+++ fileio.c
@@ -0,0 +1,16 @@
+/*
+We do any kind of I/O operation in C through a piece of data that we get in the form of a FILE* type.
+This FILE* holds all the information needed to communicate with the I/O subsystem about which file we have open and where we are in the file etc.
+This is referred to as a stream i.e a strea of data from a file or from any source
+
+FILE* name          Description
+stdin       -       Standard  inpput, generally the keyboard by default
+stdout      -       Standard output, generally the screen by default
+stderr      -       Standard Error, generally the screen by default
+
+// printf("Hello World!\n");
+    AND
+// fprintf(stdout, "Hello World!\n"); // this perfoms printf to a file
+    ARE THE SAME
+
+*/
