49434fa12f2b14772a58cd6609a1ba629027421e
sylvesterleblac-code <sylvesterleblac@gmail.com>
2026-07-28 13:52:43 +0300

    Add file I/O operations to output data to stdout in fileio.c

--- fileio.c
+++ fileio.c
@@ -162,3 +162,19 @@ in file whale.txt having whales with names, length in metres and weight in tonne
 
 
                 */
+
+                        #include<stdio.h>
+        int main() 
+        {
+            FILE *fp;
+            int x = 32;
+
+            fp = stdout
+
+            fputc('B', fp);
+            fputc('\n', fp);
+            fprintf(fp, "x = %d\n", x);
+            fputs("Hello, world!\n", fp);
+
+            fclose(fp);
+        }
