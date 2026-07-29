1a4f3fb461b7ac5658cdc4758d7de7f40a8a3937
sylvesterleblac-code <sylvesterleblac@gmail.com>
2026-07-29 11:21:54 +0300

    Auto-commit: deffingtypes.c at 7/29/2026, 11:21:54 AM

---
+++ deffingtypes.c
@@ -0,0 +1,10 @@
+/*
+we use typedef to make an alias for an existing type
+for example;
+typedef int antelope; // here we are just making antelope to be an alias for int type
+antelope x = 10; // thus antelope will do the same function as int in this assingment statemetnt
+
+typedef int antelope, bagel, mushroom; // therse are all ints
+typedef follows the scping rules including(block scope, )
+
+*/
