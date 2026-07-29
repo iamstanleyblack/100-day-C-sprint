9e722deaa2d91a0b042796b16528d4ae534d2ac1
sylvesterleblac-code <sylvesterleblac@gmail.com>
2026-07-29 11:25:50 +0300

    typedef

--- deffingtypes.c
+++ deffingtypes.c
@@ -5,6 +5,19 @@ typedef int antelope; // here we are just making antelope to be an alias for int
 antelope x = 10; // thus antelope will do the same function as int in this assingment statemetnt
 
 typedef int antelope, bagel, mushroom; // therse are all ints
-typedef follows the scping rules including(block scope, )
+typedef follows the scping rules including(block scope, file scope)
+
+how can we use the typedef? where do we apply it? 
+smetimes a struct will be typedefed to a new name so we don't need to type th eword struct over and over
+
+for example
+struct anial{
+char *name;
+int leg_count, speed;
+};
+
+typedef struct animal animal;
+struct animal y; // this will work
+anial z; // this will also work because 'animal' is just an alias
 
 */
