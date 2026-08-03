78619542c7c3930e3f89231fb63cbe4c8e1c4af9
sylvesterleblac-code <sylvesterleblac@gmail.com>
2026-08-03 12:38:52 +0300

    Auto-commit: pointers2.c at 8/3/2026, 12:38:52 PM

--- pointers2.c
+++ pointers2.c
@@ -1,8 +1,11 @@
 /*
 Adding pointers
 */
+#include<stdio.h>
+int main(){
 int a[5] = {11, 22, 33, 44, 55}
 
 // getting a pointer to the first element in the array
 int *p = &a[0];
-printf("")
+printf("%d\n", *p);
+}
