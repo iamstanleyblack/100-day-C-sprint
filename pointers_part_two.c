6abb0cf6887f46f15d9cd3fab28b5da4698bef60
sylvesterleblac-code <sylvesterleblac@gmail.com>
2026-08-03 12:39:32 +0300

    Auto-commit: pointers_part_two, pointers2.c -> pointers_part_two.c at 8/3/2026, 12:39:32 PM

--- pointers_part_two.c
+++ pointers_part_two.c
@@ -3,7 +3,7 @@ Adding pointers
 */
 #include<stdio.h>
 int main(){
-int a[5] = {11, 22, 33, 44, 55}
+int a[5] = {11, 22, 33, 44, 55};
 
 // getting a pointer to the first element in the array
 int *p = &a[0];
