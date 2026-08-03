22ecbf6028f15731a1eb20af9110c41875ec9744
sylvesterleblac-code <sylvesterleblac@gmail.com>
2026-08-03 12:47:52 +0300

    Auto-commit: pointers_part_two, pointers_part_two.c at 8/3/2026, 12:47:52 PM

--- pointers_part_two.c
+++ pointers_part_two.c
@@ -1,6 +1,5 @@
 /*
 Adding pointers
-*/
 
 #include<stdio.h>
 int main(){
@@ -14,9 +13,14 @@ printf("%d\n", *(p + 1));
 // C knows that p is a pointer to an int and so it knows the sizeof an int and it knows to skip that many bytes t get to the next int after the first one
 // Both printf("%d\n", *p); and printf("%d\n", *(p + 0)); will print 11
 printf("%d\n", *(p + 0));
-printf("We are now iterating over the elements of the array.\n")
+printf("We are now iterating over the elements of the array.\n");
 for (int i = 0; i < 5; i++) {
     printf("%d\n", *(p + i));
 }
 }
 
+How does this work?
+the memory is like a big array where byte is stored at each array index. And the array index into memory has a few names like index into memory, location, address, pointer
+---So a pointer is an index into some memory
+
+*/
