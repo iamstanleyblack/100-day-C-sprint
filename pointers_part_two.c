54223821bf17842874991ab345ed5bd30c85d87d
sylvesterleblac-code <sylvesterleblac@gmail.com>
2026-08-03 12:41:08 +0300

    Auto-commit: pointers_part_two, pointers_part_two.c at 8/3/2026, 12:41:08 PM

--- pointers_part_two.c
+++ pointers_part_two.c
@@ -1,11 +1,15 @@
 /*
 Adding pointers
 */
+
 #include<stdio.h>
 int main(){
 int a[5] = {11, 22, 33, 44, 55};
 
 // getting a pointer to the first element in the array
 int *p = &a[0];
-printf("%d\n", *p);
+printf("%d\n", *p); // this will print 11
+// adding 1 to the pointer will print the next element in the array
+printf("%d\n", *(p + 1));
 }
+
